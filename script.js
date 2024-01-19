0ace57a9bc7548a8daece4a0f07bdf4968b09409
Sushil <sushilsoni281@gmail.com>
2024-01-19 10:18:50 +0530

    update

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ let right = document.querySelector('#right');   // selecting right side of the d
 
 
 newQue.addEventListener("click", function () {
-    
+
     right.style.display = "block";
-    let rightRes=document.querySelector('#res');
-    rightRes.style.display="none";
+    let rightRes = document.querySelector('#res');
+    rightRes.style.display = "none";
     // console.log(rightRes)   
 })
 
@@ -26,11 +26,15 @@ function getData() {
     else {
         a = [];
     }
+
     if (a.length) {
         a.forEach(element => {
-            createElement(element)
-
-
+            if (element.star == true)
+                createElement(element)
+        });
+        a.forEach(element => {
+            if (element.star == false)
+                createElement(element)
         });
     }
 }
@@ -43,19 +47,20 @@ function submitQue() {
     }
     else {
         let id = Date.now();
-        console.log(id);
+        // console.log(id);
         let questionObj = {
             q: queValue, //question
             s: subValue, //subject
             Qid: id,     //question ID
-            arr:[]
+            arr: [],
+            star: false
         }
         a.push(questionObj)
         saveData(a);
         queField.value = "";
         subField.value = "";
         createElement(questionObj)
-        
+
     }
 
 
@@ -70,16 +75,69 @@ function createElement(obj) {
     let p = document.createElement("p");
     p.innerHTML = obj.q;
 
-    // let favBtn=document.createElement('button');
+    let favBtn = document.createElement('button');
+    favBtn.innerText = "\u2606";
+    favBtn.style.fontSize = "30px";
     // favBtn.className="heart";
-    // favBtn.style.backgroundColor='red';
+    if (obj.star == true) {
+        favBtn.style.color = "red";
+    }
+    else
+        favBtn.style.backgroundColor = '';
 
     //   Apendind childs in containter
     div.appendChild(h);
     div.appendChild(p);
-    // div.appendChild(favBtn);
-    cont.prepend(div)
+    div.appendChild(favBtn);
+    cont.appendChild(div)
     // window.location.reload()
+
+
+    // Adding eventlistener to the favBtn
+    favBtn.addEventListener("click", function (e) {
+        e.stopPropagation();
+        let a = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+        // favBtn.parentNode.style.display= "none";
+        let v = favBtn.parentNode.id;
+        let T = a.filter(e => {
+            return e.Qid == v
+        });
+        if (T[0].star == false) {
+            T[0].star = true;
+            console.log(T[0].star);
+            favBtn.style.color = "red";
+
+            // removeObj(a, v);
+            cont.prepend(favBtn.parentNode);
+            // a.unshift(T[0]);
+
+
+
+
+        }
+        else if (T[0].star == true) {
+            T[0].star = false;
+
+            cont.appendChild(favBtn.parentNode);
+            favBtn.style.color = "black";
+            favBtn.innerText = "\u2606";
+            favBtn.style.fontSize = "30px";
+
+
+
+
+
+        }
+        console.log(T);
+
+
+        localStorage.setItem('tasks', JSON.stringify(a));
+        // console.log(v);
+        // console.log(T[0].Qid);
+
+
+    })
+
 
 
 
@@ -113,11 +171,11 @@ function createElement(obj) {
 
 cont.addEventListener("click", function (e) {
 
-    
-    right.style.display="none";     //hides the display of right side
+
+    right.style.display = "none";     //hides the display of right side
 
     let a = e.target.parentNode.id;  //targetting id of the div we are clicking
-    //    console.log(a); 
+       console.log(a); 
     let tempArr = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
     //    console.log(tempArr)
     let newId = parseInt(a);
@@ -133,7 +191,7 @@ cont.addEventListener("click", function (e) {
 
 
     let rightArea = document.getElementById('res')
-    rightArea.innerHTML="";
+    rightArea.innerHTML = "";
     console.log(rightArea)
     let h2 = document.createElement("h2");
     h2.innerHTML = "Question";
@@ -150,46 +208,46 @@ cont.addEventListener("click", function (e) {
     let question = document.createElement('p');
     question.innerHTML = que;
     let button = document.createElement('button');
-    button.id="resolve";
+    button.id = "resolve";
     button.innerHTML = "Resolve";
 
 
     //  REMOVING ELEMENT
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function () {
         let l = document.getElementById(a);
         l.remove();
-        let right=document.getElementById('res');
-        right.innerHTML="";
+        let right = document.getElementById('res');
+        right.innerHTML = "";
         let arr = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
         console.log(arr);
-        let arr2=removeObj(arr,a);
-        localStorage.setItem('tasks',JSON.stringify(arr2));
+        let arr2 = removeObj(arr, a);
+        localStorage.setItem('tasks', JSON.stringify(arr2));
 
     })
 
-    
+
 
 
 
     // div1.innerHTML = `<h4>${sub}</h4>`;
     div1.append(subject);
     div1.append(question);
-    
+
     rightArea.append(div1);
     rightArea.append(button)
 
-    
+
     // Response section of a question
-    let h3=document.createElement('h3');
-    h3.innerHTML="Response"
-    h3.style.marginTop="36px"
+    let h3 = document.createElement('h3');
+    h3.innerHTML = "Response"
+    h3.style.marginTop = "36px"
     rightArea.append(h3)
     let div2 = document.createElement('div');
-    div2.id="responseDiv"
+    div2.id = "responseDiv"
     rightArea.append(div2);
 
-    let arrayRes=localStorage.getItem('tasks')?JSON.parse(localStorage.getItem('tasks')):[];
-    let responseArr= arrayRes[objIndex].arr;
+    let arrayRes = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+    let responseArr = arrayRes[objIndex].arr;
     console.log(responseArr)    //response array data
     // createResponse(responseArr);
 
@@ -200,87 +258,177 @@ cont.addEventListener("click", function (e) {
         });
     }
 
-    function createResponse(arr){
-        let h4=document.createElement('h4');
-        h4.innerHTML=arr.name;
-        let p=document.createElement('p');
-        p.innerHTML=arr.comment;
-        let div=document.createElement('li');
-        div.id=arr.r_id;
-        div.style.listStyleType="none";
-        div.style.border="1px solid black";
-        div.style.borderRadius="4px"
-        div.style.margin="3px 3px 3px 3px";
+    function createResponse(arr) {
+        let h4 = document.createElement('h4');
+        h4.innerHTML = arr.name;
+        let p = document.createElement('p');
+        p.innerHTML = arr.comment;
+        let div = document.createElement('li');
+        div.id = arr.r_id;
+        div.style.listStyleType = "none";
+        div.style.border = "1px solid black";
+        div.style.borderRadius = "4px"
+        div.style.margin = "3px 3px 3px 3px";
+
+        // let u = document.createElement('button');
+
         // div.style.backgroundColor=" rgb(159, 243, 243)"
-        let a=document.getElementById('responseDiv');
+        let a = document.getElementById('responseDiv');
         console.log(a);
         a.appendChild(div);
         div.appendChild(h4);
         div.appendChild(p);
 
 
+        let d=document.createElement('span');
+        // d.style.width="350px"
+        // d.style.display="flex";
+        // d.style.flexDirection="row"
+        
+        let upper = document.createElement('button');
+        upper.innerText = "🔼";
+        upper.style.width="34px";
+        let votesDisplay = document.createElement('span');
+        votesDisplay.innerText = `Upvotes: ${arr.up} | Downvotes: ${arr.down}`;
+    
+        d.appendChild(upper);
+        let down = document.createElement('button');
+        down.innerHTML = "🔽";
+        down.style.width="34px";
+        d.appendChild(down);
+        div.appendChild(d);  
+        div.appendChild(votesDisplay);
+
+
+        upper.addEventListener("click",function(){
+            arr.up++;
+            votesDisplay.innerText= `Upvotes: ${arr.up} | Downvotes: ${arr.down}`;
+            arr.netVote=arr.up-arr.down;
+            let resId=event.target.parentNode.parentElement.id;
+
+            updatelocalstorage(arr,resId);
+           
+            
+        })
+        
+        down.addEventListener("click",function(event){
+            arr.down++;
+            votesDisplay.innerText= `Upvotes: ${arr.up} | Downvotes: ${arr.down}`;
+            arr.netVote=arr.up-arr.down;
+            let resId=event.target.parentNode.parentElement.id;
+
+            console.log(resId);
+            updatelocalstorage(arr,resId);
+            
+
+
+
+        })
+    
+        // function updatelocalstorage(arrr, resId) {
+        //     console.log(arrr);
+        //     let tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+        //    let index = tasks.filter(t=>t.arr.r_id === resId);
+        //    let pIndex=tasks
+        //    console.log(index);
+                 
+           
+        // }
+        function updatelocalstorage(arrr, resId) {
+            let tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+        
+            tasks.forEach(task => {
+                let responseIndex = task.arr.findIndex(response => response.r_id == resId);
+                console.log(responseIndex);
+        
+                if (responseIndex !== -1) {
+                    task.arr[responseIndex] = arrr;
+                    task.arr.sort((a, b) => b.netVote - a.netVote);
+                    a.innerHTML="";
+                    task.arr.forEach(response => {
+                        createResponse(response);
+                    });
+                }
+                        
+                    
+            });
+           
+            
+            
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+        }
+        
+
     }
 
 
-    // Another div section where we ca add our responses
+    
+
+    
+
+
+    // Another div section where we can add our responses
     let h = document.createElement('h2');
-    h.innerText="Add Response";
-    h.style.marginTop="36px"
+    h.innerText = "Add Response";
+    h.style.marginTop = "36px"
     rightArea.append(h)
 
     let div3 = document.createElement('div');
     rightArea.append(div3);
     let input1 = document.createElement('input');
-    input1.id="nameField";  //providind id to the name box where we comment our response
-    input1.setAttribute("placeholder","Enter Your Name");
-    input1.style.width="220px";
-    input1.style.height="32px";
-    input1.style.border="1px solid black";
-    input1.style.fontSize="18px"
-    input1.style.borderRadius="5px "
+    input1.id = "nameField";  //providind id to the name box where we comment our response
+    input1.setAttribute("placeholder", "Enter Your Name");
+    input1.style.width = "220px";
+    input1.style.height = "32px";
+    input1.style.border = "1px solid black";
+    input1.style.fontSize = "18px"
+    input1.style.borderRadius = "5px "
     div3.appendChild(input1);
 
     let input2 = document.createElement('input');
-    input2.id="commentField";   //providind id to the comment box where we comment our response
-    input2.setAttribute("placeholder","Enter Comment");
-    input2.style.width="620px";
-    input2.style.height="190px";
-    input2.style.border="1px solid black";
-    input2.style.fontSize="18px"
-    input2.style.borderRadius="5px";
-    input2.style.marginTop="10px";
+    input2.id = "commentField";   //providind id to the comment box where we comment our response
+    input2.setAttribute("placeholder", "Enter Comment");
+    input2.style.width = "620px";
+    input2.style.height = "190px";
+    input2.style.border = "1px solid black";
+    input2.style.fontSize = "18px"
+    input2.style.borderRadius = "5px";
+    input2.style.marginTop = "10px";
     div3.appendChild(input2);
 
-    let commenrBtn= document.createElement("button");
-    commenrBtn.id="submitComment";
-    commenrBtn.innerHTML="Comment";
+    let commenrBtn = document.createElement("button");
+    commenrBtn.id = "submitComment";
+    commenrBtn.innerHTML = "Comment";
     rightArea.appendChild(commenrBtn);
 
 
 
     //  Adding responses on an other array using object
-    let resSubBtn= document.getElementById('submitComment');
-    let addName=document.getElementById('nameField');
-    let commentField= document.getElementById('commentField');
+    let resSubBtn = document.getElementById('submitComment');
+    let addName = document.getElementById('nameField');
+    let commentField = document.getElementById('commentField');
     // let R_id=Date.now();
     console.log(addName)
-    resSubBtn.addEventListener('click',function(){
-        let temp=localStorage.getItem('tasks')?JSON.parse(localStorage.getItem('tasks')):[];
+    resSubBtn.addEventListener('click', function () {
+        let temp = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
         let resObj = {
             name: addName.value,
             comment: commentField.value,
-            r_id : Date.now()
+            r_id: Date.now(),
+            up: 0,
+            down: 0,
+            netVote:0
         }
 
-       
-        
 
-        addName.value="";
-        commentField.value="";
-        
+
+
+        addName.value = "";
+        commentField.value = "";
+
         temp[objIndex].arr.push(resObj);
         // console.log(temp);
-        localStorage.setItem('tasks',JSON.stringify(temp));
+        localStorage.setItem('tasks', JSON.stringify(temp));
         createResponse(resObj);
         // location.reload();
         alert("Your comment is Added");
@@ -311,52 +459,41 @@ function removeObj(arr, id) {
 }
 
 
+let searchfield = document.getElementById('searchbox');
 
-let s=document.getElementById('searchbox');
+searchfield.addEventListener("input", function (event) {
+    var svalue = event.target.value.toLowerCase();
+    var matchingResults = false;
 
+    cont.innerHTML = "";  
 
-
-
-    s.addEventListener('input', e => {
-        const val = e.target.value.toLowerCase();        
-    
-        if (val !== '') {
-
-            const elements = document.querySelectorAll('#cont > div');
-            elements.forEach(element => {
-                const subject = element.querySelector('h4');
-                
-                const subjectText = subject.textContent.toLowerCase();
-    
-                if (subjectText.includes(val)) {
-                    
-                    subject.parentNode.style.backgroundColor = 'yellow';
-                    subject.style.display = 'block';
-                } else {
-                    
-                    subject.style.backgroundColor = '';
-                    
-                    subject.parentNode.style.display= 'none';
-                }
-            });
-        }
-        
-       
-        
-        else {
-            
-            const elements = document.querySelectorAll('#cont > div h4');
-            elements.forEach(subject => {
-                subject.parentNode.style.backgroundColor = '';
-                subject.parentNode.style.display='block';
-            });
+    a.forEach(function (value) {
+        if (value.q.toLowerCase().includes(svalue) || value.s.toLowerCase().includes(svalue)) {
+            createElement(value);
+            matchingResults = true;
         }
     });
-    
-    
-       
-    
-    
+
+    if (matchingResults === false && svalue.trim() === "") {
+        getData();  
+        console.log("hello data");
+    } else if (!matchingResults) {
+        cont.innerHTML = "<h3>No Results Found</h3>";
+    } else {
+        var content = cont;
+        var markInstance = new Mark(content);
+        markInstance.unmark(); 
+        markInstance.mark(svalue, { separateWordSearch: false });
+    }
+});
+
+
+
+
+
+
+
+
 
 
 
